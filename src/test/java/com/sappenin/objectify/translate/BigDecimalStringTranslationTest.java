a67b2312b710691e1a5a323bd5e1751a61077fae
David Fuelling <sappenin@gmail.com>
2012-05-17 07:47:50 -0600

    Added unit test coverage to BigDecimalStringTranslationTest

--- src/test/java/com/sappenin/objectify/translate/BigDecimalStringTranslationTest.java
+++ src/test/java/com/sappenin/objectify/translate/BigDecimalStringTranslationTest.java
@@ -26,6 +26,21 @@ public class BigDecimalStringTranslationTest extends TestBase
 		@com.sappenin.objectify.annotation.BigDecimal
 		public BigDecimal bigDecimalAmount;
 
+		@com.sappenin.objectify.annotation.BigDecimal
+		public BigDecimal bigDecimalAmountWith3Scale;
+
+		@com.sappenin.objectify.annotation.BigDecimal
+		public BigDecimal bigDecimalAmountWith2Zeros;
+
+		@com.sappenin.objectify.annotation.BigDecimal
+		public BigDecimal bigDecimalAmountWith3Zeros;
+
+		@com.sappenin.objectify.annotation.BigDecimal
+		public BigDecimal bigDecimalAmountWith4Zeros;
+
+		@com.sappenin.objectify.annotation.BigDecimal
+		public BigDecimal bigDecimalAmountWith5Zeros;
+
 		@com.sappenin.objectify.annotation.BigDecimal(encodedAmountFieldName = "eafn", displayableAmountFieldName = "dafn", indexDisplayableAmount = true, indexEncodedAmount = true, storeDisplayableAmount = true)
 		public BigDecimal bigDecimalAmountAll;
 
@@ -46,6 +61,11 @@ public class BigDecimalStringTranslationTest extends TestBase
 
 		HasBigDecimal hbd = new HasBigDecimal();
 		hbd.bigDecimalAmount = new BigDecimal("32.25");
+		hbd.bigDecimalAmountWith3Scale = new BigDecimal("23.953");
+		hbd.bigDecimalAmountWith2Zeros = new BigDecimal("23.00");
+		hbd.bigDecimalAmountWith3Zeros = new BigDecimal("75.000");
+		hbd.bigDecimalAmountWith4Zeros = new BigDecimal("86.0000");
+		hbd.bigDecimalAmountWith5Zeros = new BigDecimal("5978979.00000");
 		hbd.bigDecimalAmountAll = new BigDecimal("1856465.25");
 		hbd.bigDecimalAmountAllNoIndices = new BigDecimal("54648979841");
 		hbd.bigDecimalAmountNoDisplayableAmount = new BigDecimal("54648979841.25");
@@ -54,6 +74,42 @@ public class BigDecimalStringTranslationTest extends TestBase
 
 		assert hbd.bigDecimalAmount.equals(fetched.bigDecimalAmount);
 		assert hbd.bigDecimalAmountAll.equals(fetched.bigDecimalAmountAll);
+		assert hbd.bigDecimalAmountWith3Scale.equals(fetched.bigDecimalAmountWith3Scale);
+		assert hbd.bigDecimalAmountWith2Zeros.equals(fetched.bigDecimalAmountWith2Zeros);
+		assert hbd.bigDecimalAmountWith3Zeros.equals(fetched.bigDecimalAmountWith3Zeros);
+		assert hbd.bigDecimalAmountWith4Zeros.equals(fetched.bigDecimalAmountWith4Zeros);
+		assert hbd.bigDecimalAmountWith5Zeros.equals(fetched.bigDecimalAmountWith5Zeros);
+		assert hbd.bigDecimalAmountAllNoIndices.equals(fetched.bigDecimalAmountAllNoIndices);
+		assert hbd.bigDecimalAmountNoDisplayableAmount.equals(fetched.bigDecimalAmountNoDisplayableAmount);
+
+	}
+	
+	@Test
+	public void testBigDecimalStringConverterUsingIntConstructor() throws Exception
+	{
+		fact.getTranslators().add(new BigDecimalStringTranslatorFactory());
+		fact.register(HasBigDecimal.class);
+
+		HasBigDecimal hbd = new HasBigDecimal();
+		hbd.bigDecimalAmount = new BigDecimal(3225);
+		hbd.bigDecimalAmountWith3Scale = new BigDecimal(23.953f);
+		hbd.bigDecimalAmountWith2Zeros = new BigDecimal(23.00);
+		hbd.bigDecimalAmountWith3Zeros = new BigDecimal(75.000);
+		hbd.bigDecimalAmountWith4Zeros = new BigDecimal(86.0000);
+		hbd.bigDecimalAmountWith5Zeros = new BigDecimal(5978979.00000);
+		hbd.bigDecimalAmountAll = new BigDecimal(1856465.25);
+		hbd.bigDecimalAmountAllNoIndices = new BigDecimal(546489798);
+		hbd.bigDecimalAmountNoDisplayableAmount = new BigDecimal(54648979841.25);
+
+		HasBigDecimal fetched = this.putClearGet(hbd);
+
+		assert hbd.bigDecimalAmount.equals(fetched.bigDecimalAmount);
+		assert hbd.bigDecimalAmountAll.equals(fetched.bigDecimalAmountAll);
+		assert hbd.bigDecimalAmountWith3Scale.equals(fetched.bigDecimalAmountWith3Scale);
+		assert hbd.bigDecimalAmountWith2Zeros.equals(fetched.bigDecimalAmountWith2Zeros);
+		assert hbd.bigDecimalAmountWith3Zeros.equals(fetched.bigDecimalAmountWith3Zeros);
+		assert hbd.bigDecimalAmountWith4Zeros.equals(fetched.bigDecimalAmountWith4Zeros);
+		assert hbd.bigDecimalAmountWith5Zeros.equals(fetched.bigDecimalAmountWith5Zeros);
 		assert hbd.bigDecimalAmountAllNoIndices.equals(fetched.bigDecimalAmountAllNoIndices);
 		assert hbd.bigDecimalAmountNoDisplayableAmount.equals(fetched.bigDecimalAmountNoDisplayableAmount);
 
